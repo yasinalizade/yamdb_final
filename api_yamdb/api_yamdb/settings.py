e2a3af66f19bae5429d10ed5e1b32d2b134df2a0
Ясин Ализаде <yasin.alizade@yandex.ru>
2022-07-03 13:53:25 +0300

    add corrections

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR + '/api/static/']
+# STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
